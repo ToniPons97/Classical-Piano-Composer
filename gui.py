7dbb15fc942eaba960764f8bb3579e4efeb8ff38
Antonio Pons <toninopons@gmail.com>
2019-12-17 11:38:52 +0100

    fumer

--- gui.py
+++ gui.py
@@ -1,13 +1,33 @@
-import os
-from tkinter import Tk
-import tkFileDialog
-toplevel = Tk()
-toplevel.withdraw()
-filename = tkFileDialog.askopenfilename()
-if os.path.isfile(filename):
-    for line in open(filename,'r'):
-        print line,
+import tkinter as tk
+from tkinter import ttk
+from os import listdir
+from os.path import isfile, join
 
-else: print 'No file chosen'
 
-raw_input('Ready, push Enter')
+
+midi_files = [f for f in listdir("./midi_songs2") if isfile(join("./midi_songs2", f))]
+
+sound_fonts = [f for f in listdir("../../.fluidsynth") if isfile(join("../../.fluidsynth", f))]
+
+
+
+app = tk.Tk() 
+app.geometry('370x370')
+
+labelTop = tk.Label(app,
+                    text = "Choose your favourite month")
+labelTop.grid(column=0, row=0)
+
+comboMidis = ttk.Combobox(app, 
+                            values = midi_files)
+comboMidis.grid(column=0, row=1)
+comboMidis.current(1)
+
+comboSoundFonts = ttk.Combobox(app, 
+                            values = sound_fonts)
+comboSoundFonts.grid(column=1, row=1)
+comboSoundFonts.current(1)
+
+
+app.mainloop()
+
